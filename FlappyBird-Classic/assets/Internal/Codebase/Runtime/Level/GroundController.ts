48b238bf22b70b107ba558c5162d2e3651254f1d
Rimuru <Rimuru.Dev@gmail.com>
2023-12-30 14:11:01 +0400

    Update GroundController.ts

--- FlappyBird-Classic/assets/Internal/Codebase/Runtime/Level/GroundController.ts
+++ FlappyBird-Classic/assets/Internal/Codebase/Runtime/Level/GroundController.ts
@@ -7,8 +7,10 @@ export class GroundController extends Component {
 
     @property({ type: Node })
     public groundOne: Node;
+
     @property({ type: Node })
     public groundTwo: Node;
+
     @property({ type: Node })
     public groundThree: Node;
 
@@ -19,30 +21,32 @@ export class GroundController extends Component {
     private tempStartLocations: Vec3[] = [];
 
     protected onLoad(): void {
-        this.initGrounds();
+
+        [this.groundOne, this.groundTwo, this.groundThree].forEach((ground, index) => {
+
+            const width = ground.getComponent(UITransform).width;
+
+            this.groundWidths.push(width);
+            this.tempStartLocations.push(new Vec3(index * width, 0, 0));
+
+            ground.setPosition(this.tempStartLocations[index]);
+        });
     }
 
     protected update(deltaTime: number): void {
+
         const delta: number = this.gameSpeed * deltaTime;
         const canvasWidth = director.getScene().getComponentInChildren(Canvas).getComponent(UITransform).width;
 
         this.tempStartLocations.forEach((location, index) => {
+
             location.x -= delta;
-            if (location.x <= (-this.groundWidths[index])) {
+            if (location.x <= (-this.groundWidths[index]))
                 location.x = canvasWidth;
-            }
+
             this.groundOne.setPosition(this.tempStartLocations[0]);
             this.groundTwo.setPosition(this.tempStartLocations[1]);
             this.groundThree.setPosition(this.tempStartLocations[2]);
-        });
-    }
-
-    private initGrounds(): void {
-        [this.groundOne, this.groundTwo, this.groundThree].forEach((ground, index) => {
-            const width = ground.getComponent(UITransform).width;
-            this.groundWidths.push(width);
-            this.tempStartLocations.push(new Vec3(index * width, 0, 0));
-            ground.setPosition(this.tempStartLocations[index]);
         });
     }
 }
